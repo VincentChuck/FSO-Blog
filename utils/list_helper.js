fb2169b41abfe5abf338588f2e17141b4fff8e4d
Vincent Chuck <vincent.chuck7@gmail.com>
2022-10-21 21:01:08 +0800

    Commit exercises 4.6 - 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,40 @@
+const _ = require('lodash')
 const totalLikes = (blogs) => {
     return blogs.reduce( (sum, blog) => sum + blog.likes, 0)
 }
 
 const favoriteBlog = (blogs) => {
     const maxLike = Math.max(...blogs.map(o => o.likes))
-    const favBlog = blogs.find(o => o.likes === maxLike)
+    // const favBlog = blogs.find(o => o.likes === maxLike)
+    const favBlog = _.maxBy(blogs, 'likes')
     const {__v, _id, url, ...output} = favBlog
     return output
 }
 
+const mostBlogs = (blogs) => {
+    const output = _(blogs)
+        .countBy('author')
+        .entries()
+        .maxBy(_.last)
+    return {author: output[0], blogs: output[1]}
+}
+
+const mostLikes = (blogs) => {
+    const likes = Object.create(null)
+
+    blogs.forEach(blog => {
+        likes[blog.author] = (likes[blog.author] || 0) + blog.likes
+    }) 
+
+    const mostLikes = _(likes)
+        .entries()
+        .maxBy(_.last)
+    return {author: mostLikes[0], likes: mostLikes[1]}
+}
+
 module.exports = {
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
