4bedbe0daa56890c99bd85d29665a3d3bee54203
Vincent Chuck <vincent.chuck7@gmail.com>
2022-10-21 17:51:38 +0800

    Commit exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const totalLikes = (blogs) => {
     return blogs.reduce( (sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const maxLike = Math.max(...blogs.map(o => o.likes))
+    const favBlog = blogs.find(o => o.likes === maxLike)
+    const {__v, _id, url, ...output} = favBlog
+    return output
+}
+
 module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
