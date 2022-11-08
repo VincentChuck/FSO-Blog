7d9a867d11e8531c77244a027461ca33ca884da6
Vincent Chuck <vincent.chuck7@gmail.com>
2022-11-08 16:19:05 +0800

    Commit exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+
   blog.likes = blog.likes || 0
+  const usersInDb = await User.find({})
+  const firstUser = usersInDb[0]
+  console.log(firstUser)
+  blog.user = firstUser._id
 
   const savedBlog = await blog.save()
+  firstUser.blogs = firstUser.blogs.concat(savedBlog._id)
+  await firstUser.save()
+
   response.status(201).json(savedBlog)
 })
 
--- models/user.js
+++ models/user.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   username: String,
   name: String,
-  passwordHash: String,
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Blog'
     }
-  ],
+  ]
 })
 
 userSchema.set('toJSON', {
