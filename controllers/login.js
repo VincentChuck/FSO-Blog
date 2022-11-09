7d8f31c79df907297c3e4059d13886be9e345cd3
Vincent Chuck <vincent.chuck7@gmail.com>
2022-11-09 21:07:55 +0800

    Commit exercise 4.20

--- controllers/login.js
+++ controllers/login.js
@@ -25,7 +25,7 @@ loginRouter.post('/', async (request, response) => {
   const token = jwt.sign(
     userForToken,
     process.env.SECRET,
-    { expiresIn: 30 } //token expires in 60*60 seconds = 1 hour
+    { expiresIn: 60*60 } //token expires in 60*60 seconds = 1 hour
   )
 
   response
